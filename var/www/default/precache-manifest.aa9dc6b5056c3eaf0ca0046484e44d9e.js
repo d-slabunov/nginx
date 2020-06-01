2443c5e2fa341c66032bf0a6cdf62a086d406863
Danila Slabunov <d.slabunov.dev@gmail.com>
2020-06-01 19:38:02 +0300

    Add regional mode

---
+++ var/www/default/precache-manifest.aa9dc6b5056c3eaf0ca0046484e44d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a59a7a87f5c200f6293668cc349cb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "691027dff783ca00b253",
+    "url": "./static/css/main.19e4fd6f.chunk.css"
+  },
+  {
+    "revision": "af77f3ac48bac9bc07f5",
+    "url": "./static/js/2.95e010a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.95e010a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691027dff783ca00b253",
+    "url": "./static/js/main.beb87bd9.chunk.js"
+  },
+  {
+    "revision": "85489f2d0e8853851972",
+    "url": "./static/js/runtime-main.c6581807.js"
+  }
+]);
---
+++ var/www/veneto/precache-manifest.5b6ffcbc7b6076b17ee2512afd6c7a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58206f054934e9b8f4ace8d9afbb70e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8788589b38a59702a808",
+    "url": "./static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "0bb00c6718e6abe9be61",
+    "url": "./static/js/2.e2a3be29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e2a3be29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8788589b38a59702a808",
+    "url": "./static/js/main.2c661da0.chunk.js"
+  },
+  {
+    "revision": "09147165108a6f5632eb",
+    "url": "./static/js/runtime-main.8726a1de.js"
+  }
+]);
